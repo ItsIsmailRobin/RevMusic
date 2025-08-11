2475e12657fcc90c8122c0815455609e553a5a42
Revenger_xD <87826486+ItsIsmailRobin@users.noreply.github.com>
2025-08-11 13:53:15 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,9 +12,9 @@ const bricolage_grotesque = Bricolage_Grotesque({
 });
 
 export const metadata = {
-  title: "MusicHub",
-  description: "Open-Source music streamer.",
-  icons: "/favi-icon.jpg",
+  title: "RevMusic",
+  description: "Next.js 14 Music Web - Rev666.",
+  icons: "/fav/favicon.png",
   manifest: "/manifest.json",
 };
 
